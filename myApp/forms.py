bb30cd852305d6671d9a24e787c6b5cd03212104
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-18 14:20:39 +0300

    add filters for rate, genre and release_year

--- myApp/forms.py
+++ myApp/forms.py
@@ -1,25 +1,31 @@
 from django import forms
-from .models import Rating
-from django.core.validators import MinValueValidator, MaxValueValidator
-
+from .models import Rating, Genre
+from django.utils import timezone
 
 
 class RatingForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ('rate', 'review')
-        
-        
+
+
 class FilterMovieForm(forms.Form):
     filter_rate = forms.DecimalField(
-        validators=[MaxValueValidator(5), MinValueValidator(0)],         
-        decimal_places=1,
-        max_digits=2,
-        required=False)   
-       
-    # This is how we change the lable or placeholder for a field
+            min_value=0,
+            max_value=5,
+            decimal_places=1,
+            max_digits=2,
+            required=False,
+            label='Movies with the rate greater than:',
+            widget=forms.TextInput(attrs={'placeholder': 'Between(0-5) Ex. 3.8'}))
+    genre = forms.ChoiceField(required=False)
+    release_year = forms.ChoiceField(
+            choices= [('', 'All Years')] + [(i, i) for i in range(1950, timezone.now().year + 1)],
+            required=False)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['filter_rate'].label = 'Movies with the rate greater than:'
-        self.fields['filter_rate'].widget.attrs['placeholder'] = 'Between(0-5) Ex. 3.8'
-    
+        try:
+            self.fields['genre'].choices =  [('', 'All Genres')] + [(g.id, g.title) for g in Genre.objects.all()]
+        except:
+            self.fields['genre'] = []
--- myApp/views.py
+++ myApp/views.py
@@ -5,25 +5,37 @@ from .models import Rating, Movie, Genre
 from .forms import RatingForm, FilterMovieForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.db.models import FloatField, Avg
+from django.db.models.functions import Round
 
 
 class IndexView(generic.ListView):
     model = Movie
     template_name = "myApp/index.html"
-    
+
     def get_context_data(self, **kwargs):
-        context = {}
+        context = super().get_context_data(**kwargs)
+
         form = FilterMovieForm(self.request.GET)
         context['form'] = form
+
+        movies = Movie.objects.annotate(
+                    rating=Round(Avg('ratings__rate'), 1, output_field=FloatField()))
+
         if form.is_valid():
             rate = self.request.GET.get('filter_rate', None)
-            if rate:
-                # __gte means grater than or equal
-                context['movies'] = Movie.objects.filter(average_rating__gte=rate)
-            else:
-                context['movies'] = Movie.objects.all()
-        else:
-            context['movies'] = Movie.objects.all()  
+            if rate is not None:
+                movies = movies.filter(rating__gte=rate)
+
+            genre = self.request.GET.get('genre', None)
+            if genre:
+                movies = movies.filter(genre_id=genre)
+
+            release_year = self.request.GET.get('release_year', None)
+            if release_year:
+                movies = movies.filter(release_year__gte=release_year)
+
+        context['movies'] = movies
         return context
 
 
@@ -32,7 +44,7 @@ class RateView(LoginRequiredMixin, generic.CreateView):
     model = Rating
     form_class = RatingForm
     template_name = "myApp/rating.html"
-    
+
     def get_context_data(self, **kwargs):
         """Just to show the name of the movie at top of the page:)"""
         context = super().get_context_data(**kwargs)
@@ -44,11 +56,11 @@ class RateView(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         """Prevent rate duplication and refresh the average rate filed."""
         movie_id = self.kwargs.get('pk')
-        movie = get_object_or_404(Movie, pk=movie_id) 
-        # Check if the user rated this movie before delete it.       
+        movie = get_object_or_404(Movie, pk=movie_id)
+        # Check if the user rated this movie before delete it.
         rating_exists = Rating.objects.filter(movie_id=movie_id, user_id=self.request.user.id)
         if rating_exists:
-            rating_exists.delete()        
+            rating_exists.delete()
         form.instance.user = self.request.user
         form.instance.movie = movie
         form.save()
@@ -56,7 +68,7 @@ class RateView(LoginRequiredMixin, generic.CreateView):
         # Update the average_rating field.
         movie.get_average_rating()
         return redirect('myApp:home')
-    
+
     # TODO: Or a better form_valid:
     # def form_valid(self, form):
     # movie_id = self.kwargs.get('pk')
@@ -65,11 +77,10 @@ class RateView(LoginRequiredMixin, generic.CreateView):
     # # Update if exists, or create a new entry
     # rating, created = Rating.objects.update_or_create(
     #     movie=movie, user=self.request.user,
-    #     defaults={'rate': form.cleaned_data['rate']}  
+    #     defaults={'rate': form.cleaned_data['rate']}
     # )
     # if created:
     #     messages.success(self.request, "Rating added successfully!")
     # else:
     #     messages.success(self.request, "Rating updated successfully!")
     # return redirect('myApp:home')
-      
